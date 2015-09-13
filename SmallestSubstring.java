fa1d6f3be3811eed9f160f178e4baa826fd84639
Bryan Tamada <btamada@users.noreply.github.com>
2015-09-12 17:22:52 -0700

    Updated algorithm

--- SmallestSubstring.java
+++ SmallestSubstring.java
@@ -25,86 +25,60 @@ public class SmallestSubstring {
 	
 	private static String getShortestUniqueSubstring(char[] arr, String str) {
 			
-		/*
-		 * TODO
-		 * 1) Create a pointer that keeps track of the first unique character
-		 * 2) Create a pointer that keeps track of the last unique character found
-		 * 3) Iterate through "str" to search for a sequence of unique characters.
-		 * 	  If we find a repeating character then reset the points and start all
-		 * 	  over again until we either find the unique sequence of characters or
-		 *    we iterate through all of the characters of "str."
-		 * 4) Save the unique characters in the string "result"
-		 * 5) If str.equalsIgnoreCase(result) then we found our substring and
-		 *    we return it in the result variable or else we return null;
-		 */
 		if(arr == null || str == null || arr.length >= str.length()) return null;
 		
-		String result = null;
-		int begIndex = 0;
-		int endIndex = 0;
-		int begCounter = 0;
-		int endCounter = 0;
+		HashMap<Character,Integer> hm = new HashMap<>();
 		
-		HashMap<Character, Integer> hm = new HashMap<>();
+		char[] strCA = str.toCharArray();
 		
-		// Time O(m)
-		// Space O(m)
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 0; i < arr.length; ++i) {
 			hm.put(arr[i], 0);
 		}
 		
-		// Time O(n)
-		for(int i = 1; i < str.length(); i++) {
-			/*
-			 * Subtract consecutive characters to determine if they are the same
-			 * value by checking if the result is equal to 0 (zero).  If so, then
-			 * skip to the next iteration and increment the begIndex and endIndex
-			 * pointers.
-			 */
+		int strLength = str.length();
+		
+		int previous = 0;
+		
+		String result = "";
+		
+		for(int i = 0; i < strLength; ++i) {
 			
-			if(str.charAt(begIndex) - str.charAt(endIndex) == 0) {
-				if(endIndex - begIndex > 1) {
-					for(int j = begIndex; j < endIndex; j++)
-					{
-						hm.put(str.charAt(j), 0);
-					}
-				} else {
-					hm.put(str.charAt(begIndex), 1);
+			// if we find a character not in the hashmap then skip to the next iteration
+			if(hm.get(strCA[i]) == null) continue;
+			
+			// if we find a character already in the hashmap then zero out our previous counters
+			// in this case it would never be greater than 1 because we have a check here
+			if(hm.get(strCA[i]) == 1) {
+				
+				// zero out the previous counters
+				for(int j = previous; j < i; ++j) {
+					hm.put(strCA[j], 0);
 				}
-				begIndex = i-1;
-				endIndex = i;
-				continue;
-			}
-			
-			begCounter = hm.get(str.charAt(begIndex));
-			endCounter = hm.get(str.charAt(endIndex));
-			
-			if(begCounter == 0 && endCounter == 0) {
-				hm.put(str.charAt(begIndex), 1);
-				hm.put(str.charAt(endIndex), 1);
-				result += str.charAt(begIndex);
-				result += str.charAt(endIndex);
-				endIndex = ++i;
-			} else {
-				endCounter += hm.put(str.charAt(i), ++endCounter);
-				if(endCounter > 1) {
-					hm.put(str.charAt(begIndex), 0);
-					hm.put(str.charAt(i), 1);
-					begIndex = i;
-					endIndex = ++i;
-					result = str.charAt(i) + "";
-				} else if (endCounter == 0) {
-					hm.put(str.charAt(endIndex), 1);
-					result += str.charAt(endIndex);
-					endIndex = ++i;
+				
+				result = "";
+				
+				// we need to decide where to set the previous pointer
+				// and how to set the result string
+				if(strCA[i-1] == strCA[i]) {
+					previous = i;
+					result += Character.toString(strCA[i]);
 				} else {
-					result += str.charAt(endIndex);
-					endIndex = ++i;
+					previous = i-1;
+					hm.put(strCA[previous],1);
+					result += Character.toString(strCA[previous]);
 				}
 			}
+			
+			// if the character value is 0...
+			// increment the the associated key's value in the hashmap
+			hm.put(strCA[i], 1);
+			
+			// add the character to our result string
+			result += strCA[i];
 		}
 		
-		return result.equalsIgnoreCase(str) ? result : null;
+		// do a last check if the result is the same as the unique characters in our array
+		return result;
 	}
 
 }
