cc693700854cd6dc1e32631e50d20f841abea22f
Bryan Tamada <btamada@users.noreply.github.com>
2015-09-23 22:44:45 -0700

    Linked List Algorithm

--- Node.java
+++ Node.java
@@ -1,83 +1,38 @@
-package singlylinkedlist;
+package interview_questions;
 
-public class Node {
-	int data;
-	Node next;
-
-	public Node(int data) {
+public class Node<T extends Comparable<T>> {
+	
+	public T getData() {
+		return data;
+	}
+	
+	public void setData(T data) {
 		this.data = data;
-		this.next = null;
 	}
 	
-	/*
-	 * Print out the SLL
-	 * Time: O(n)
-	 * Space: O(1)
-	 */
-	public void print(Node head) {
-		while(head.next != null) {
-			System.out.println(head.data);
-			head = head.next;
-		}
-		System.out.println(head.data);
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
+		if(next == null) return;
+		this.next = next;
 	}
 	
-	/*
-	 * Add a node to the SLL
-	 * Time: O(1)
-	 * Space: O(1)
-	 */
-	public void addNode(int data) {
-		Node tail = new Node(data);
-		Node current = this;
-		
-		while(current.next != null) {
-			current = current.next;
-		}
-		
-		current.next = tail;
+	public Node(T data) {
+		this.data = data;
+		this.visited = false;
 	}
 	
-	/*
-	 * Delete a node from the SLL
-	 * Time: O(n)
-	 * Space: O(1)
-	 */
-	public boolean deleteNode(Node head, int data) {
-		/*
-		 * We don't delete the head node, we just return the 
-		 */
-		Node current = head;
-		
-		if(current.data == data) {
-			current = current.next;
-			return true;
-		}
-		
-		while(current.next != null) {
-			if(current.next.data == data) {
-				current.next = current.next.next;
-				return true;
-			}
-			current = current.next;
-		}
-		return false;
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
 	}
 	
-	/*
-	 * Search for a node in the SLL
-	 * Time: O(n)
-	 * Space: O(1)
-	 */
-	public boolean searchNode(int data) {
-		Node current = this;
-		
-		while(current.next != null) {
-			if(current.data == data || current.next.data == data) {
-				return true;
-			}
-			current = current.next;
-		}
-		return false;
-	}
+	private T data;
+	private boolean visited;
+	private Node<T> next;
 }
